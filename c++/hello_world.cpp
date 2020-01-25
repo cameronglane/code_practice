3f096e794a95a38db777f8dba3fed7b179f1ea11
Cameron Lane <cam@cameronlane.dev>
2020-01-25 04:06:52 -0800

    Creating new practice code repo.

---
+++ c++/hello_world.cpp
@@ -0,0 +1,15 @@
+// TODO(cam@cameronlane.dev): Delete this, I guess...
+
+#include <iostream>
+#include "hello_world.h"
+
+int main() {
+  print_message();
+
+  return 0;
+}
+
+void print_message() {
+  printf("Hello, world!\n");
+  return;
+}
